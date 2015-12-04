5fc282c687865e1472f74423301b7acb16383087
oroborusm <juan.bustamante.dg@gmail.com>
2015-12-04 14:59:48 -0800

    imagen bosch

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20151030033336) do
   create_table "forms", force: :cascade do |t|
     t.string   "nombre",     limit: 255
     t.string   "email",      limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "telefono",   limit: 255
   end
 
